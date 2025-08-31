8914342c1964ffbd1ac4b7772c38878b419aa583
Rei <philippeandreid@gmail.com>
2025-08-31 15:04:44 +0800

    feat: simplify ChatsPage by redirecting to the first chat user

--- wired/app/chats/page.tsx
+++ wired/app/chats/page.tsx
@@ -1,14 +1,9 @@
-import ChatWindow from "../../components/ChatWindow";
 import { chatUsers } from "../../lib/chatData";
-import { useState } from "react";
-
+import { redirect } from "next/navigation";
 
 export default function ChatsPage() {
-  const [selectedChat, setSelectedChat] = useState(chatUsers[0].id);
-  const user = chatUsers.find((u) => u.id === selectedChat);
-  return (
-    <div className="flex flex-col gap-8 p-8">
-      {user && <ChatWindow user={user.name} messages={user.messages} />}
-    </div>
-  );
+  if (chatUsers.length > 0) {
+    redirect(`/chats/${chatUsers[0].id}`);
+  }
+  return null;
 }
--- wired/app/page.tsx
+++ wired/app/page.tsx
@@ -3,12 +3,6 @@ import { redirect } from "next/navigation";
 import ChatInterface from "@/components/ChatInterface";
 
 export default async function Home() {
-  const { userId } = await auth();
-  
-  // If the user is not logged in, redirect to the sign-in page
-  if (!userId) {
-    redirect("/sign-in");
-  }
-  
-  return <ChatInterface />;
+  redirect("/chats");
+  return null;
 }
